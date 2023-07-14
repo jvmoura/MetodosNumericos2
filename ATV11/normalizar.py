6b214b13666682dd231b95515bf667c6e9521b91
João Vitor Moura <83800365+jvmoura@users.noreply.github.com>
2023-07-13 23:35:32 -0300

    Delete ATV11 directory

--- ATV11/normalizar.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import scipy
-
-def normalizar(vetorK_velho, vetorT):
-  vetor = vetorK_velho/(scipy.linalg.sqrtm(np.matmul(vetorT, vetorK_velho), disp=True, blocksize=64))
-  return vetor  
